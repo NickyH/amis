457032b0587a809cc5ca9e6e3b61ba705d5d5d82
NickyH <nicky.hughes@tapestry.net>
2013-10-10 10:31:06 +1100

    tabs working

--- js/override.js
+++ js/override.js
@@ -1,6 +1,30 @@
-$(document).delegate('[data-role="navbar"] a', 'click', function () {
-    $(this).addClass('ui-btn-active');
-    $('.content_div').hide();
-    $('#' + $(this).attr('data-href')).show();
-    return false;//stop default behavior of link
+$(function(){
+
+  $(".tab-bar .tab").on('click', show_active_tab);
+  $('[data-role="navbar"] a').on('click', change_tab_content);
+  $("#show-tab").on('click', show_active_tab);
+  $("#show-tab").trigger('click');
+
 });
+
+// add tab-active class to tab on click
+function show_active_tab()
+{
+  $(this).addClass("tab-active");
+  event.stopPropagation();
+  $(this).siblings().removeClass("tab-active");
+}
+
+// change content on tab click
+function change_tab_content()
+{
+  console.log('content');
+  $(this).addClass('ui-btn-active');
+  $('.content_div').hide();
+  $('#' + $(this).attr('data-href')).show();
+  return false;//stop default behavior of link
+}
+
+
+
+
