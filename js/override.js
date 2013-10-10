2ebca987654f32f922e976eb7371b64734539926
NickyH <nicky.hughes@tapestry.net>
2013-10-10 12:14:17 +1100

    added openlayers map

--- js/override.js
+++ js/override.js
@@ -1,10 +1,11 @@
 $(function(){
 
   $(".tab-bar .tab").on('click', show_active_tab);
+  $("#details-link").on('click', show_active_tab);
   $('[data-role="navbar"] a').on('click', change_tab_content);
   $("#show-tab").on('click', show_active_tab);
   $("#show-tab").trigger('click');
-
+  insert_map();
 });
 
 // add tab-active class to tab on click
@@ -18,13 +19,16 @@ function show_active_tab()
 // change content on tab click
 function change_tab_content()
 {
-  console.log('content');
   $(this).addClass('ui-btn-active');
   $('.content_div').hide();
   $('#' + $(this).attr('data-href')).show();
   return false;//stop default behavior of link
 }
 
-
-
-
+function insert_map()
+{
+  console.log('map!!!!');
+  $.get('map_openlayer.html', function(data) {
+    $('#insert-map').html(data);
+    });
+}
